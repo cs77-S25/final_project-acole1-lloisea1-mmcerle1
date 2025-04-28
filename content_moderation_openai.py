615f43f15f84f50c3757460ba9ba87f01c914fd6
Morgan McErlean <mcerlean.morgan@gmail.com>
2025-04-28 01:07:17 +0100

    Create content_moderation_openai.py

---
+++ content_moderation_openai.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+import requests
+import os
+
+with open('api_key.txt', 'r') as file:
+    openai_apikey = file.read().strip()
+
+os.environ['OPENAI_API_KEY'] = openai_apikey
+
+client = OpenAI()
+ 
+def moderate_text(input_text):
+    print("in moderate text")
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {openai_apikey}',
+    }
+    data = {'input': input_text}
+    response = client.moderations.create(
+        model="omni-moderation-latest",
+        input= input_text,
+    )
+    result = response.json()
+
+    print(result)
+
+    print("Result", result[len(result)-5])
+
+    if result[len(result)-5] == "u":
+        print('Content blocked: Violates usage policies.')
+        return True
+    else:
+        print('Content allowed.')
+        return False
