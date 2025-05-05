9cbe48312690f137852ef837471220c8a74c2daf
Morgan McErlean <mcerlean.morgan@gmail.com>
2025-05-05 21:10:44 +0100

    Update blog.js

--- static/js/blog.js
+++ static/js/blog.js
@@ -5,6 +5,9 @@ const list = document.getElementById('item-list')
 const itemWidth = 150
 const padding = 10
 
+
+// Scroll functions: The two functions below are
+// code from scrolling the blog post carousel
 prev.addEventListener('click',()=>{
   list.scrollLeft -= itemWidth + padding
 })
@@ -12,5 +15,4 @@ prev.addEventListener('click',()=>{
 next.addEventListener('click',()=>{
   list.scrollLeft += itemWidth + padding
 })
-
 
